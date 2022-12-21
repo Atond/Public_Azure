b45dc8e4c29341b9f40097adf7fa5c2957467366
Atond <max_max_max5@hotmail.fr>
2022-12-21 18:58:04 +0100

    Add files via upload

---
+++ qwinsta.ps1
@@ -0,0 +1,32 @@
+﻿function Get-TSSessions {
+    param(
+        $ComputerName = 'localhost'
+    )
+    qwinsta /server:$ComputerName |
+    #Parse output
+    ForEach-Object {
+        $_.Trim() -replace '\s+', ','
+    } |
+    #Convert to objects
+    ConvertFrom-Csv
+}
+$count = 0
+
+$query = Get-TSSessions
+
+foreach ($q in $query) {
+    if (($q.ID -like '*D*co*') -and ($q.SESSION -notlike 'services')) {
+        rwinsta $q.UTILISATEUR
+    }
+    if ($q.ÉTAT -like 'Actif') {
+        $count ++
+    }
+    if ($count -eq 0) {
+        $state = 'OFF'
+    }
+    else {
+        $state = 'ON'
+    }
+}
+
+$state
